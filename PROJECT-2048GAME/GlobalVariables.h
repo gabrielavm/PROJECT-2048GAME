1aaa7f598daafdcd744f1f4de805ca29f0e7c899
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-07-15 01:38:23 +0300

    Update GlobalVariables.h
    Fixing constants

--- PROJECT-2048GAME/GlobalVariables.h
+++ PROJECT-2048GAME/GlobalVariables.h
@@ -1,28 +1,28 @@
 #pragma once
 
-char filename1[] = "Leaderboard4x4.txt";
-char filename2[] = "Leaderboard5x5.txt";
-char filename3[] = "Leaderboard6x6.txt";
-char filename4[] = "Leaderboard7x7.txt";
-char filename5[] = "Leaderboard8x8.txt";
-char filename6[] = "Leaderboard9x9.txt";
-char filename7[] = "Leaderboard10x10.txt";
+const char filename1[] = "Leaderboard4x4.txt";
+const char filename2[] = "Leaderboard5x5.txt";
+const char filename3[] = "Leaderboard6x6.txt";
+const char filename4[] = "Leaderboard7x7.txt";
+const char filename5[] = "Leaderboard8x8.txt";
+const char filename6[] = "Leaderboard9x9.txt";
+const char filename7[] = "Leaderboard10x10.txt";
 
-char scoreList1[] = "scoreList1.txt";
-char scoreList2[] = "scoreList2.txt";
-char scoreList3[] = "scoreList3.txt";
-char scoreList4[] = "scoreList4.txt";
-char scoreList5[] = "scoreList5.txt";
-char scoreList6[] = "scoreList6.txt";
-char scoreList7[] = "scoreList7.txt";
+const char scoreList1[] = "scoreList1.txt";
+const char scoreList2[] = "scoreList2.txt";
+const char scoreList3[] = "scoreList3.txt";
+const char scoreList4[] = "scoreList4.txt";
+const char scoreList5[] = "scoreList5.txt";
+const char scoreList6[] = "scoreList6.txt";
+const char scoreList7[] = "scoreList7.txt";
 
-char usernameList1[] = "usernameList1.txt";
-char usernameList2[] = "usernameList2.txt";
-char usernameList3[] = "usernameList3.txt";
-char usernameList4[] = "usernameList4.txt";
-char usernameList5[] = "usernameList5.txt";
-char usernameList6[] = "usernameList6.txt";
-char usernameList7[] = "usernameList7.txt";
+const char usernameList1[] = "usernameList1.txt";
+const char usernameList2[] = "usernameList2.txt";
+const char usernameList3[] = "usernameList3.txt";
+const char usernameList4[] = "usernameList4.txt";
+const char usernameList5[] = "usernameList5.txt";
+const char usernameList6[] = "usernameList6.txt";
+const char usernameList7[] = "usernameList7.txt";
 
 const int SIZE = 100;
 char username[SIZE][SIZE] = {};
